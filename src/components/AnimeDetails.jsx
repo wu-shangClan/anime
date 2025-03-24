ffd25d093af5b8c0a329fcfd5eb66515af402149
amit <pandheramit245@gmail.com>
2025-03-24 15:11:11 +0100

    init

--- src/components/AnimeDetails.jsx
+++ src/components/AnimeDetails.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
-import AnimeCard from './AnimeCard'
+
 
 const AnimeDetails = () => {
   const { id } = useParams()
